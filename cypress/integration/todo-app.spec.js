ac423fde41c8a267484a03c979052d72c89f9cdf
Rajiv Singh <singh.rajiv@bcg.com>
2021-08-23 15:07:04 -0700

    Added code to include specific accessibility rules

--- cypress/integration/todo-app.spec.js
+++ cypress/integration/todo-app.spec.js
@@ -20,6 +20,12 @@ describe('Todo application', () => {
     })
 
     it('shoud only include rules with serious and critical impacts', () => {
-        cy.checkA11y9(null, { includedImpacts: ['critical', 'serious']})
+        cy.checkA11y(null, { includedImpacts: ['critical', 'serious']})
+    })
+
+    it('shoud exclude specific accessibilitiy rules', () => {
+        cy.checkA11y(null, { rules: {
+            'color-contrast': { enabled: false}
+        }})
     })
 })
